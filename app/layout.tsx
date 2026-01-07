0052d012dbb3eb7c6fca82aa49c11a444684fe98
feloshxhs <mitarbeita.mitarbeita@gmx.ch>
2026-01-07 17:30:59 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,15 +6,50 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Bitpanda - Web3 Wallet',
-  description: 'A secure, easy-to-use decentralized Web3 wallet for seamless multi- chain management, letting you manage all your addresses and assets in one place.',
-  generator: 'crypto,web3,kucoin,bot,trading,wallet,exchange',
+  description:
+    'A secure, easy-to-use decentralized Web3 wallet for seamless multi-chain management, letting you manage all your addresses and assets in one place.',
+
+  keywords: [
+    'crypto wallet',
+    'web3 wallet',
+    'multi-chain wallet',
+    'decentralized wallet',
+    'blockchain',
+  ],
+
+  icons: {
+    icon: [
+      {
+        url: 'https://i.ibb.co/fzgXwJtF/Bild-2026-01-07-171848938.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        url: 'https://i.ibb.co/fzgXwJtF/Bild-2026-01-07-171848938.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        url: 'https://i.ibb.co/fzgXwJtF/Bild-2026-01-07-171848938.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    apple: [
+      {
+        url: 'https://i.ibb.co/fzgXwJtF/Bild-2026-01-07-171848938.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
@@ -24,3 +59,4 @@ export default function RootLayout({
     </html>
   )
 }
+
